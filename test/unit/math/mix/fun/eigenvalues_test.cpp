0589b922fa4bce852676f25daeb13706d391d4d7
Brian Ward <bward@flatironinstitute.org>
2022-06-15 11:44:42 -0400

    Attempt 2 at complex eigendecomposition

--- test/unit/math/mix/fun/eigenvalues_test.cpp
+++ test/unit/math/mix/fun/eigenvalues_test.cpp
@@ -1,18 +1,24 @@
 #include <test/unit/math/test_ad.hpp>
 #include <stdexcept>
+#include <stan/math/prim/fun/to_complex.hpp>
 
 TEST(mathMixFun, eigenvalues) {
+  using stan::math::to_complex;
+  stan::test::ad_tolerances tols = stan::test::reverse_only_ad_tolerances();
+
   auto f = [](const auto& x) {
     using stan::math::eigenvalues;
     return eigenvalues(x);
   };
   for (const auto& x : stan::test::square_test_matrices(0, 2)) {
     stan::test::expect_ad(f, x);
+    stan::test::expect_ad(tols, f, to_complex(x, 0).eval());
   }
 
   Eigen::MatrixXd a32(3, 2);
   a32 << 3, -5, 7, -7.2, 9.1, -6.3;
   EXPECT_THROW(f(a32), std::invalid_argument);
+  EXPECT_THROW(f(to_complex(a32, 0)), std::invalid_argument);
 }
 
 // see eigenvectors_test.cpp for test of eigenvectors() and eigenvalues()
--- test/unit/math/mix/fun/eigenvectors_test.cpp
+++ test/unit/math/mix/fun/eigenvectors_test.cpp
@@ -1,54 +1,25 @@
 #include <test/unit/math/test_ad.hpp>
 #include <stdexcept>
+#include <stan/math/prim/fun/to_complex.hpp>
 
 TEST(mathMixFun, eigenvectors) {
+  using stan::math::to_complex;
+  stan::test::ad_tolerances tols = stan::test::reverse_only_ad_tolerances();
+
   auto f = [](const auto& x) {
     using stan::math::eigenvectors;
     return eigenvectors(x);
   };
   for (const auto& x : stan::test::square_test_matrices(0, 2)) {
     stan::test::expect_ad(f, x);
+    stan::test::expect_ad(tols, f, to_complex(x, 0).eval());
   }
 
   Eigen::MatrixXd a32(3, 2);
   a32 << 3, -5, 7, -7.2, 9.1, -6.3;
   EXPECT_THROW(f(a32), std::invalid_argument);
+  EXPECT_THROW(f(to_complex(a32, 0)), std::invalid_argument);
 }
 
-template <typename T>
-void expect_identity_matrix(const T& x) {
-  EXPECT_EQUAL(x.rows(), x.cols());
-  for (int j = 0; j < x.cols(); ++j) {
-    for (int i = 0; i < x.rows(); ++i) {
-      EXPECT_NEAR(i == j ? 1 : 0, x(i, j), 1e-6);
-    }
-  }
-}
-
-template <typename T>
-void expectEigenvectorsId() {
-  for (const auto& m_d : stan::test::square_test_matrices(0, 2)) {
-    Eigen::Matrix<T, -1, -1> m(m_d);
-    auto vecs = eigenvectors(m).eval();
-    auto vals = eigenvalues(m).eval();
-    auto I = (vecs.inverse() * m * vecs * vals.asDiagonal().inverse()).real();
-    expect_identity_matrix(I);
-  }
-}
-
-// THESE TESTS USED TO WORK STANDALONE
-
-TEST(mathMixFun, eigenvectorsId) {
-  using d_t = double;
-  using v_t = stan::math::var;
-  using fd_t = stan::math::fvar<double>;
-  using ffd_t = stan::math::fvar<fd_t>;
-  using fv_t = stan::math::fvar<stan::math::var>;
-  using ffv_t = stan::math::fvar<fv_t>;
-
-  // expectEigenvectorsId<v_t>();
-  // expectEigenvectorsId<fd_t>();
-  // expectEigenvectorsId<ffd_t>();
-  // expectEigenvectorsId<fv_t>();
-  // expectEigenvectorsId<ffv_t>();
-}
+// see eigenvectors_test.cpp for test of eigenvectors() and eigenvectors()
+// using reconstruction identities
